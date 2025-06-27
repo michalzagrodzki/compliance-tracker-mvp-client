0fd2340c72eec954417611d790da4a9ac6b26118
michal zagrodzki <michalzagrodzki@gmail.com>
2025-06-27 08:57:53 +0200

    feat: remove unused console.log

--- src/modules/audit/components/AuditSessionDocuments.tsx
+++ src/modules/audit/components/AuditSessionDocuments.tsx
@@ -237,7 +237,6 @@ export default function AuditSessionDocuments({ sessionId }: AuditSessionDocumen
       </Card>
     )
   }
-  console.log(sessionDocuments)
   return (
     <Card>
       <CardHeader>
